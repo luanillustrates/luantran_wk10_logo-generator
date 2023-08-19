24bd39713f14f4ce14ff176a566f3bcf01663ae4
luanillustrates <luanillustrates@gmail.com>
2023-08-19 16:33:46 +0930

    added new js files

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const inquirer = require('inquirer');
+const createLogo = require('./lib/createLogo.js')
 const { writeFile } = require('fs').promises;
 
 const logoPrompt = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'usage',
-            message: 'describe the usage of the project',
+            name: 'name',
+            message: 'Enter a name (max 3 characters)',
         },
+        {
+            type: 'input',
+            name: 'textcolour',
+            message: 'Enter a colour for the text',
+        },        
         {
             type: 'list',
             name: 'shape',
             choices: ['square', 'circle', 'triangle'],
-            message: 'pick a shape',
+            message: 'Pick a shape from the following',
         },
+        {
+            type: 'input',
+            name: 'shapecolour',
+            message: 'Enter a colour for the shape',
+        },  
     ])
 }
+
+const init = () => {
+    logoPrompt()
+        .then((answers) => writeFile('./examples/logo.svg', createLogo(answers)))
+        .then(() => console.log('successfully created .svg'))
+        .catch((err) => console.error(err));
+}
+
+init();
