7564410d21c3e9eb7e129276d5a45a0f75254c69
luanillustrates <luanillustrates@gmail.com>
2023-08-20 22:48:36 +0930

    updated classes and chains

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 const inquirer = require('inquirer');
-const createLogo = require('./lib/createLogo.js')
-const { writeFile } = require('fs').promises;
+const fs = require('fs');
+const createLogo = require('./lib/createLogo.js');
+const createShapes = require('./lib/createShapes.js');
 
-const logoPrompt = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter a name (max 3 characters)',
-        },
-        {
-            type: 'input',
-            name: 'textcolour',
-            message: 'Enter a colour for the text',
-        },        
-        {
-            type: 'list',
-            name: 'shape',
-            choices: ['square', 'circle', 'triangle'],
-            message: 'Pick a shape from the following',
-        },
-        {
-            type: 'input',
-            name: 'shapecolour',
-            message: 'Enter a colour for the shape',
-        },  
-    ])
-}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'logoName',
+                message: 'Enter a name (max 3 characters)',
+            },
+            {
+                type: 'input',
+                name: 'textColour',
+                message: 'Enter a colour or hex number for the logo text',
+            },
+            {
+                type: 'list',
+                name: 'logoShape',
+                choices: ['square', 'circle', 'triangle'],
+                message: 'Pick a shape from the following',
+            },
+            {
+                type: 'input',
+                name: 'shapeColour',
+                message: 'Enter a colour or hex number for the shape colour',
+            },
+        ])
+        .then((answers) => {
+            const generatedLogo = createShapes(answers);
 
-const init = () => {
-    logoPrompt()
-        .then((answers) => writeFile('./examples/logo.svg', createLogo(answers)))
-        .then(() => console.log('successfully created .svg'))
+            fs.writeFile('./examples/logo.svg', createLogo(generatedLogo), (err) => err ? console.error(err) : console.log('generated logo.sgv')
+            );
+
+        })
         .catch((err) => console.error(err));
 }
 
